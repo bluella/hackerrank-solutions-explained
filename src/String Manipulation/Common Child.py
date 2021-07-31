89fbf0b5b446e16429127dbd582e44c336f972f5
Igor Grigorev <barsic1080@gmail.com>
2021-07-31 20:51:16 +0300

    Add common child

--- src/String Manipulation/Common Child.py
+++ src/String Manipulation/Common Child.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """
 https://www.hackerrank.com/challenges/common-child
-A string is said to be a child of a another string if it can be formed 
-by deleting 0 or more characters from the other string. 
-Letters cannot be rearranged. Given two strings of equal length, 
-what's the longest string that can be constructed such that it is a child of both? 
+A string is said to be a child of a another string if it can be formed
+by deleting 0 or more characters from the other string.
+Letters cannot be rearranged. Given two strings of equal length,
+what's the longest string that can be constructed such that it is a child of both?
 """
 
 import math
@@ -12,31 +12,34 @@ import os
 import random
 import re
 import sys
-from collections import Counter
 
 
-def makeAnagram(s1, s2):
+def commonChild(s1, s2):
     """
     Args:
         s1 (str): first string
         s2 (str): second string
 
     Returns:
-        int: how many elements need to be deleted"""
-    # counter is going to create a dict with char: freq for each char from string
-    # counter works as sets (with the hashing) so it's very fast
-    counter_a = Counter(s1)
-    counter_b = Counter(s2)
-    # calculate number of elements to eliminate
-    diff1 = counter_a - counter_b
-    diff2 = counter_b - counter_a
-    return sum(diff1.values()) + sum(diff2.values())
+        int: common child max len"""
+    prev = [0] * (len(s2) + 1)
+    curr = [0] * (len(s2) + 1)
+
+    # loop each string
+    # increase max common child count if common char is found
+    for s1_char in s1:
+        for j, s2_char in enumerate(s2, 1):
+            curr[j] = prev[j - 1] + \
+                1 if s1_char == s2_char else max(prev[j], curr[j - 1])
+        prev, curr = curr, prev
+
+    return prev[-1]
 
 
 if __name__ == "__main__":
 
     ex_s1 = 'abc'
-    ex_s2 = 'dsvcba'
+    ex_s2 = 'sdgab'
 
-    result = makeAnagram(ex_s1, ex_s2)
+    result = commonChild(ex_s1, ex_s2)
     print(result)
